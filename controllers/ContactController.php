8a2078a723b66ffa04489121226dd317ff6f5d1a
WilD <wild.ajc1@gmail.com>
2025-07-28 15:57:58 +0200

    POST plus robuste. Vérification du format du N°tel de l'email. Vérification si email/N°tel deja utilisés par un contact déja enregistreé dans la base

--- controllers/ContactController.php
+++ controllers/ContactController.php
@@ -59,14 +59,53 @@ class ContactController
             return;
         }
 
+        if (!filter_var($input['email'], FILTER_VALIDATE_EMAIL)){
+            http_response_code(400);
+            echo json_encode(['error'=> 'email non valide ']);
+            return;
+        }
+
+         //Validation du numéro de téléphone (France : 10 chiffres, commence par 0 ou format international)
+         //Je me demandais où placer cette fonction car pas adapté pour le modèle et je latrouve génante pour le controlleur 
+         // Il semblerait que par convention il faut la mettre dasn un fichier helper/utilitaire ou service validation . Je le ferai plus tard 
+        function estNumTelephoneValide($numero) {
+            $num = preg_replace('/[\s\-.]/', '', $numero);
+            // 0XXXXXXXXX ou +33XXXXXXXXX
+            if (preg_match('/^0[1-9][0-9]{8}$/', $num)) return true; //regex
+            if (preg_match('/^\+33[1-9][0-9]{8}$/', $num)) return true;
+            return false;
+        }
+
+        if (!estNumTelephoneValide($input['phone'])) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Numéro de téléphone invalide (format attendu : 06XXXXXXXX ou +336XXXXXXXX)']);
+            return;
+        }
+
         $contact = new Contact();
+        $contactsList= $contact->findAll();
+
+        //J'utilise un foreach mais je sais que je pourrais trés bien creer une fonction findbyemail dans le modèle et l'appeler ça serait plus propre 
+        //je n'en ai pas besoin pour le moment je veux 'abord finir mon modèle 
+        foreach ($contactsList as $contact ) {
+            if($contact['email']===$input['email']){
+                http_response_code(409);//conflit
+                echo json_encode(['error' => 'Email deja utilisé']);
+                return;
+            }
+            if ($contact['phone'] === $input['phone']) {
+                http_response_code(409); // Conflit
+                echo json_encode(["error" => "Numéro de téléphone déjà utilisé"]);
+                return;
+            }
+        }
 
         try {
             $newId = $contact->insert($input);
 
             http_response_code(201); // Création réussie
             echo json_encode([
-                "message" => "Contact créé avec succès",
+                "message" => "Nouveau contact".$input['lastname']." ". $input['firstname'] . "créé avec succès",
                 "id" => $newId
             ]);
         } catch (Exception $e) {
